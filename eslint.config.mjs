c1b14f3cd87ece08b0e8316b9dd04bd6cee8c614
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2025-02-11 16:47:31 -0300

    feat: initial project settings

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      "react/react-in-jsx-scope": "off", // Desabilita a regra
+      indent: ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+    },
+  },
+];
