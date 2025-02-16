b5a77301e3422d5a87d6b53bff99a668b5665383
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2025-02-16 09:50:27 -0300

    feat: add TableModal component

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { Button } from "../components/Button";
 import { Categories } from "../components/Categories";
 import { Header } from "../components/Header";
 import { Menu } from "../components/Menu";
+import { TableModal } from "../components/TableModal";
 import {
   Container,
   CategoriesContainer,
@@ -11,6 +13,12 @@ import {
 } from "./styles";
 
 export function Main() {
+  const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+  const [selectedTable, setSelectedTable] = useState("");
+
+  function handleSaveTable(table: string) {
+    setSelectedTable(table);
+  }
   return (
     <>
       <Container>
@@ -26,9 +34,18 @@ export function Main() {
       </Container>
       <Footer>
         <FooterContainer>
-          <Button onPress={() => {}}>Novo pedido</Button>
+          {!selectedTable && (
+            <Button onPress={() => setIsTableModalVisible(true)}>
+              Novo pedido
+            </Button>
+          )}
         </FooterContainer>
       </Footer>
+      <TableModal
+        visible={isTableModalVisible}
+        onClose={() => setIsTableModalVisible(false)}
+        onSave={handleSaveTable}
+      />
     </>
   );
 }
